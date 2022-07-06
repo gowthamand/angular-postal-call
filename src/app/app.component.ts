13ffc3946783726655e9f00c29760e3c536d2b9a
Gowthaman Duraisamy -X (gowtdura - MIRAFRA SOFTWARE TECHNOLOGIES PVT LTD at Cisco) <gowtdura@cisco.com>
2022-07-06 17:59:00 +0530

    design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,17 @@ import {PostalService} from "./postal.service";
 export class AppComponent {
   name = 'Angular';
   public posts: any;
-
+  public submit = true
   constructor(private postalService: PostalService) {
 
   }
 
   continue() {
+    this.submit = false
     const pocode: HTMLInputElement = document.getElementById('postalcode') as HTMLInputElement;
     if (pocode.value) {
       this.postalService.getPosteData(pocode.value).subscribe(res => {
+        this.submit = true;
         this.posts = res[0].PostOffice;
         console.log(res[0].PostOffice);
       });
